e0ff46d06db271629f2846c08d29d7baf5c9b243
nickzap872 <167074229+nickzap872@users.noreply.github.com>
2024-07-15 14:43:57 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    const hq = 42; // Scuber's headquarters on 42nd Street
+    return Math.abs(someValue - hq);
+  }
+  
+  function distanceFromHqInFeet(someValue) {
+    const blocks = distanceFromHqInBlocks(someValue);
+    const feetPerBlock = 264;
+    return blocks * feetPerBlock;
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+    const blocksTravelled = Math.abs(destination - start);
+    const feetPerBlock = 264;
+    return blocksTravelled * feetPerBlock;
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
+  
